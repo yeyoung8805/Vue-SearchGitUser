652a5ce2ebe250e1cb0ba933716ab04e0c78f5f6
yeyoung8805 <yyk8805@naver.com>
2022-03-03 17:43:01 +0900

    edit search/result Field (not working)

--- src/components/ResultField.vue
+++ src/components/ResultField.vue
@@ -1,12 +1,50 @@
 <template>
     <div>
-        ResultField
+        <ul>
+            <li v-for="(item) in items" v-bind:key="item">
+                <span>{{item.login}}</span>
+                <span>{{item.avatar_url}}</span>
+                <span>{{item.html_url}}</span>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-
+    components: {},
+    data() {
+        return {
+            items: [],
+            total_count : 0,
+        }
+    },
+    methods: {
+        read() {
+            let params = {
+                'q' : items.item,
+            }
+            axios
+                .get("https://api.github.com/search/users", {
+                    headers : {"accept" : "application/vnd.github.v3+json"},
+                    params: {params}
+                })
+                .then(response => {
+                    const{data} = response
+                    console.log(data)
+                    this.items = data.items
+                    this.total_count = data.total_count
+                })
+                .catch(error => {
+                    alert(error)
+                })
+        }
+    },
+    created() {
+        console.log(axios)
+        this.read()
+    },
 }
 </script>
 
--- src/components/SearchField.vue
+++ src/components/SearchField.vue
@@ -1,12 +1,32 @@
 <template>
     <div>
-        SearchField
+        <input type="text" v-model="newSearchItem" v-on:keyup.enter="searchItem">
+        <span v-on:click="searchItem">add
+        </span>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            newSearchItem: ""
+        }
+    },
+    methods: {
+        searchItem: function() {
+            if(this.newSearchItem!= '') {
+                var obj = {completed: false, item: this.newSearchItem};
+                console.log(this.newSearchItem);
+                //저장하는 로직
+                localStorage.setItem(this.newSearchItem, JSON.stringify(obj)); //스트링으로 변경되도록 함
+                this.clearInput();
+            }
+        },
+        clearInput: function() {
+            this.newSearchItem = '';
+        }
+    },
 }
 </script>
 
