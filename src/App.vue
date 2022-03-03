679bd94c2dfa2c44cd775753eca1cf38dea89f58
yeyoung8805 <yyk8805@naver.com>
2022-03-03 15:02:44 +0900

    add components file

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <SearchField></SearchField>
+    <ResultField></ResultField>
+  </div>
 </template>
+
+<script>
+import SearchField from './components/SearchField.vue'
+import ResultField from './components/ResultField.vue'
+
+export default {
+  components: {
+    'SearchField' : SearchField,
+    'ResultField' : ResultField
+  }
+}
+</script>
+
 
 <style>
 #app {
@@ -15,16 +27,5 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/ResultField.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        ResultField
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
